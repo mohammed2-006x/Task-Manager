f371bae96e125368a136f1664a27a91714218462
Mohammed Taher <mohammedtaher2.006x@gmail.com>
2026-04-18 01:44:25 +0200

    Initialize project and add core functionality

---
+++ Form1.Designer.cs
@@ -0,0 +1,253 @@
+﻿namespace Task_Manager
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btnAddTask = new System.Windows.Forms.Button();
+            this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+            this.checkedListBox2 = new System.Windows.Forms.CheckedListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblCompletedTasks = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.Image = global::Task_Manager.Properties.Resources.Task_Manage_Icon_removebg_preview;
+            this.pictureBox1.Location = new System.Drawing.Point(130, -3);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(77, 92);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox2.Image = global::Task_Manager.Properties.Resources.Task_Manager_removebg_preview;
+            this.pictureBox2.Location = new System.Drawing.Point(-40, -3);
+            this.pictureBox2.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(213, 92);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 5;
+            this.pictureBox2.TabStop = false;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Trebuchet MS", 20F);
+            this.textBox1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.textBox1.Location = new System.Drawing.Point(72, 126);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(551, 38);
+            this.textBox1.TabIndex = 6;
+            this.textBox1.Text = "Type Your Task Here...";
+            this.textBox1.MouseEnter += new System.EventHandler(this.textBox1_MouseEnter);
+            this.textBox1.MouseLeave += new System.EventHandler(this.textBox1_MouseLeave);
+            // 
+            // btnAddTask
+            // 
+            this.btnAddTask.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(170)))), ((int)(((byte)(0)))), ((int)(((byte)(250)))));
+            this.btnAddTask.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.btnAddTask.FlatAppearance.BorderSize = 4;
+            this.btnAddTask.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Red;
+            this.btnAddTask.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Fuchsia;
+            this.btnAddTask.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnAddTask.Font = new System.Drawing.Font("Trebuchet MS", 26F);
+            this.btnAddTask.Location = new System.Drawing.Point(629, 126);
+            this.btnAddTask.Name = "btnAddTask";
+            this.btnAddTask.Size = new System.Drawing.Size(68, 55);
+            this.btnAddTask.TabIndex = 7;
+            this.btnAddTask.Text = "+";
+            this.btnAddTask.UseVisualStyleBackColor = false;
+            this.btnAddTask.Click += new System.EventHandler(this.btnAddTask_Click);
+            // 
+            // checkedListBox1
+            // 
+            this.checkedListBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(10)))), ((int)(((byte)(10)))), ((int)(((byte)(10)))));
+            this.checkedListBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.checkedListBox1.Font = new System.Drawing.Font("Tahoma", 20F);
+            this.checkedListBox1.ForeColor = System.Drawing.Color.Cornsilk;
+            this.checkedListBox1.FormattingEnabled = true;
+            this.checkedListBox1.Location = new System.Drawing.Point(72, 188);
+            this.checkedListBox1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.checkedListBox1.Name = "checkedListBox1";
+            this.checkedListBox1.Size = new System.Drawing.Size(625, 387);
+            this.checkedListBox1.Sorted = true;
+            this.checkedListBox1.TabIndex = 4;
+            this.checkedListBox1.SelectedIndexChanged += new System.EventHandler(this.checkedListBox1_SelectedIndexChanged);
+            // 
+            // checkedListBox2
+            // 
+            this.checkedListBox2.BackColor = System.Drawing.Color.Black;
+            this.checkedListBox2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.checkedListBox2.Enabled = false;
+            this.checkedListBox2.Font = new System.Drawing.Font("Tahoma", 20F, System.Drawing.FontStyle.Strikeout);
+            this.checkedListBox2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(170)))), ((int)(((byte)(0)))), ((int)(((byte)(250)))));
+            this.checkedListBox2.FormattingEnabled = true;
+            this.checkedListBox2.Location = new System.Drawing.Point(703, 188);
+            this.checkedListBox2.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.checkedListBox2.Name = "checkedListBox2";
+            this.checkedListBox2.Size = new System.Drawing.Size(353, 387);
+            this.checkedListBox2.TabIndex = 8;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(170)))), ((int)(((byte)(0)))), ((int)(((byte)(250)))));
+            this.label1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.label1.Font = new System.Drawing.Font("Trebuchet MS", 26F);
+            this.label1.Location = new System.Drawing.Point(741, 146);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(280, 43);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Completed Tasks";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Black;
+            this.label2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.label2.Font = new System.Drawing.Font("Trebuchet MS", 26F);
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(170)))), ((int)(((byte)(0)))), ((int)(((byte)(250)))));
+            this.label2.Location = new System.Drawing.Point(749, 581);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(214, 43);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Completed :";
+            // 
+            // lblCompletedTasks
+            // 
+            this.lblCompletedTasks.AutoSize = true;
+            this.lblCompletedTasks.BackColor = System.Drawing.Color.Black;
+            this.lblCompletedTasks.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.lblCompletedTasks.Font = new System.Drawing.Font("Trebuchet MS", 26F);
+            this.lblCompletedTasks.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(170)))), ((int)(((byte)(0)))), ((int)(((byte)(250)))));
+            this.lblCompletedTasks.Location = new System.Drawing.Point(964, 581);
+            this.lblCompletedTasks.Name = "lblCompletedTasks";
+            this.lblCompletedTasks.Size = new System.Drawing.Size(37, 43);
+            this.lblCompletedTasks.TabIndex = 11;
+            this.lblCompletedTasks.Tag = "0";
+            this.lblCompletedTasks.Text = "0";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Lime;
+            this.button1.Font = new System.Drawing.Font("Trebuchet MS", 18F);
+            this.button1.Location = new System.Drawing.Point(72, 596);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(159, 46);
+            this.button1.TabIndex = 12;
+            this.button1.Text = "Select All";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Red;
+            this.button2.Font = new System.Drawing.Font("Trebuchet MS", 18F);
+            this.button2.Location = new System.Drawing.Point(305, 596);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(159, 46);
+            this.button2.TabIndex = 13;
+            this.button2.Text = "Remove All";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.Blue;
+            this.button3.Font = new System.Drawing.Font("Trebuchet MS", 18F);
+            this.button3.Location = new System.Drawing.Point(538, 596);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(159, 46);
+            this.button3.TabIndex = 14;
+            this.button3.Text = "Remove";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::Task_Manager.Properties.Resources.istockphoto_947365206_170667a;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1126, 676);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.lblCompletedTasks);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.checkedListBox2);
+            this.Controls.Add(this.btnAddTask);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.checkedListBox1);
+            this.Font = new System.Drawing.Font("Trebuchet MS", 8F);
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "Form1";
+            this.Text = " Task Manager";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btnAddTask;
+        private System.Windows.Forms.CheckedListBox checkedListBox1;
+        private System.Windows.Forms.CheckedListBox checkedListBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblCompletedTasks;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Task_Manager
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+
+        private void textBox1_MouseEnter(object sender, EventArgs e)
+        {
+          
+            if(textBox1.Text== "Type Your Task Here...")
+            textBox1.Text = "";
+        }
+
+        private void textBox1_MouseLeave(object sender, EventArgs e)
+        {
+            textBox1.Text= textBox1.Text.TrimStart();
+
+
+            if (textBox1.Text =="")
+            {
+                checkedListBox1.Focus();
+                textBox1.Text = "Type Your Task Here...";
+            }
+        }
+
+        private void btnAddTask_Click(object sender, EventArgs e)
+        {
+            
+            if(textBox1.Text.Trim()!="" && textBox1.Text!= "Type Your Task Here...")
+            {
+                checkedListBox1.Items.Add(textBox1.Text.Trim());
+                textBox1.Text = "Type Your Task Here...";
+            }
+        }
+
+        private void checkedListBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            for (int i = 0; i < checkedListBox1.Items.Count; i++)
+            {
+                if (checkedListBox1.GetItemChecked(i))
+                {
+                    checkedListBox2.Items.Add(checkedListBox1.Items[i]);
+                    checkedListBox2.SetItemChecked(checkedListBox2.Items.Count - 1, true);
+                    checkedListBox1.Items.RemoveAt(i);
+                    i--;
+
+                    lblCompletedTasks.Tag = (Convert.ToInt16(lblCompletedTasks.Tag) + 1).ToString();
+                    lblCompletedTasks.Text=lblCompletedTasks.Tag.ToString();
+
+                }
+
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < checkedListBox1.Items.Count; i++)
+            {
+
+                checkedListBox1.SetItemChecked(i, true);
+              
+            }
+            checkedListBox1_SelectedIndexChanged(checkedListBox1, e);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            for (int i = checkedListBox1.Items.Count-1; i >=0; i--)
+            {
+
+                checkedListBox1.Items.RemoveAt(i);
+
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            for(int i=0;i<checkedListBox1.Items.Count;i++)
+            {
+                if (checkedListBox1.GetSelected(i))
+                {
+                    checkedListBox1.Items.RemoveAt(i);
+                }
+            }
+        }
+    }
+}
